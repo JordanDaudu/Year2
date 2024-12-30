86b50c18fd6eb7cb0686dfedf1e22bd54f97b1f1
Jordan <jordandaudu@gmail.com>
2024-12-30 12:48:42 +0200

    Create main.py

---
+++ Numerical_Analysis/Lagrange_Interpolation/main.py
@@ -0,0 +1,44 @@
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+def lagrange_interpolation(x_data, y_data, x):
+    """
+    Lagrange Interpolation
+
+    Parameters:
+    x_data (list): List of x-values for data points.
+    y_data (list): List of y-values for data points.
+    x (float): The x-value where you want to evaluate the interpolated polynomial.
+
+    Returns:
+    float: The interpolated y-value at the given x.
+    """
+    n = len(x_data)
+    result = 0.0
+
+    for i in range(n):
+        term = y_data[i]
+        for j in range(n):
+            if i != j:
+                term *= (x - x_data[j]) / (x_data[i] - x_data[j])
+        result += term
+
+    return result
+
+if __name__ == '__main__':
+
+    x_data = [1, 2, 5]
+    y_data = [1, 0, 2]
+    x_interpolate = 3  # The x-value where you want to interpolate
+    y_interpolate = lagrange_interpolation(x_data, y_data, x_interpolate)
+    print(bcolors.OKBLUE, "\nInterpolated value at x =", x_interpolate, "is y =", y_interpolate, bcolors.ENDC)
+
